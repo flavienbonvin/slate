46f3d38204b9cb7c028dbce5b3fc417f55e9d8d1
Flavien Bonvin <fbonvin@MacBook-Pro-Perso.local>
2025-04-22 21:18:30 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <body className="container pt-10 h-screen mx-auto bg-gray-100 dark:bg-gray-950 text-black dark:text-white">
+      <h1>Start typing</h1>
+    </body>
+  );
+}
+
+export default App;
