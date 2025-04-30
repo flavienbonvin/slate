5287c3f6d0748e63817294511844bc3ee0053dc2
Flavien Bonvin <flavien.bonvin@pm.me>
2025-04-30 21:59:37 +0200

    Fix scroll in view when writing and save button position

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -15,6 +15,8 @@ export const Editor = () => {
         extensions: [StarterKit],
         content,
         editorProps: {
+            scrollThreshold: { top: 0, right: 0, bottom: 80, left: 0 },
+            scrollMargin: { top: 0, right: 0, bottom: 80, left: 0 },
             attributes: {
                 class: "prose prose-headings:text-current dark:prose-headings:text-current dark:prose-code:text-current mx-auto text-neutral-950 focus:outline-none dark:text-neutral-300",
             },
@@ -36,16 +38,13 @@ export const Editor = () => {
     return (
         <div className="flex min-h-screen w-full flex-col">
             {status === "saved" && (
-                <div className="animate-fade-in-out absolute top-0 right-0 mr-2 flex items-center">
+                <div className="animate-fade-in-out sticky top-0 mr-2 flex items-center self-end">
                     <Dot className="text-lime-600 dark:text-lime-400" />
                     <p className="text-sm text-neutral-950 dark:text-neutral-300">Saved</p>
                 </div>
             )}
 
-            <main
-                className="prose mx-auto w-full flex-1 px-10 pt-10 pb-16 md:px-0"
-                onClick={handleClick}
-            >
+            <main className="prose mx-auto w-full flex-1 p-10 md:px-0" onClick={handleClick}>
                 <EditorContent
                     onClick={handleClick}
                     onFocus={handleFocus}
